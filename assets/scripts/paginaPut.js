744b49da89adfe01c10503d154e9f1b4e0889e6b
luanatiuma <luanatiuma@gmail.com>
2023-07-14 11:50:44 -0300

    Desafio da página put

--- assets/scripts/paginaPut.js
+++ assets/scripts/paginaPut.js
@@ -1,3 +1,5 @@
+
+
 document.querySelector('#btAtualizar').addEventListener('click', () => {
 
     const dados = {
@@ -30,20 +32,46 @@ fetch('http://localhost:3000/produtos', {
     headers: {
         'Content-type': 'application/json'
     }
-})
+}) //criação da lista de produtos
     .then(resposta => resposta.json())
     .then(resposta => {
         
         for(let i = 0; i < resposta.length; i++) {
 
             const ul = document.createElement('ul');
+            ul.id = resposta[i].id;
             ul.classList.add('produto');
 
-            ul.appendChild(document.createElement('li')).innerHTML = resposta[i].id;
-            ul.appendChild(document.createElement('li')).innerHTML = resposta[i].descricao;
-            ul.appendChild(document.createElement('li')).innerHTML = resposta[i].preco;
-            
-            document.querySelector('#listaProdutos').appendChild(ul);
-        }
-        
+            const liId = document.createElement('li');
+            liId.setAttribute('data-produto', 'id');
+            liId.innerHTML = resposta[i].id;
+            liId.classList.add('produto-id');
+
+            const liDescricao = document.createElement('li');
+            liDescricao.setAttribute('data-produto', 'descricao');
+            liDescricao.innerHTML = resposta[i].descricao;
+
+            const liPreco = document.createElement('li');
+            liPreco.setAttribute('data-produto', 'preco');
+            liPreco.innerHTML = resposta[i].preco;
+
+            ul.append(liId, liDescricao, liPreco);
+
+            document.querySelector('#listaProdutos').appendChild(ul)
+        }   
     });
+
+    //Desafio 1 - clicar nos produtos e os valores irem para o formulário para fazer a atualização
+document.querySelector('#listaProdutos' /*lista produtos é o id da ul onde estão os lis com os produtos*/).addEventListener('click', evento => {
+    //console.log(evento.target); -> pode user sempre o console log pra verificar para onde o evento esta direcionado
+    if(evento.target.closest('ul')/*Pode também ser colocado parentElement que será o pai do elemento */.classList.contains('produto')) {
+        //console.log(evento.target);
+        const elementoBase = evento.target.closest('ul'); //transformando a ul numa constante para facilitar
+
+        document.querySelector('input#id').value = elementoBase.querySelector('[data-produto="id"]'/*para seletionar os atributos coloca-se []*/).innerHTML;
+
+        document.querySelector('input#descricao').value = elementoBase.querySelector('[data-produto="descricao"]'/*para seletionar os atributos coloca-se []*/).innerHTML;
+
+        document.querySelector('input#preco').value = elementoBase.querySelector('[data-produto="preco"]'/*para seletionar os atributos coloca-se []*/).innerHTML;
+    }
+});
